17041b6b799c556ac2abb9c5bb287e0b932052df
j Martin Povis <martin25zxc@gmail.com>
2020-11-16 16:14:20 -0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.css";
+import Layout from "./Layout";
+function App() {
+  return (
+    <React.Fragment>
+      <Layout>HELLO WORLD!!!</Layout>
+    </React.Fragment>
+  );
+}
+
+export default App;
